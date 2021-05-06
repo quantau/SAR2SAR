b631bc7c27bbf407bcec3a26414d2818c0fba07a
emanueledalsasso <emanuele.dalsasso@outlook.com>
2021-05-06 13:06:45 +0200

    added checkpoint

--- Code/SAR2SAR-test/u_net.py
+++ Code/SAR2SAR-test/u_net.py
@@ -24,54 +24,54 @@ def autoencoder(x, width=256, height=256, **_kwargs):
     skips = [x]
 
     n = x
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv0', kernel_regularizer=regularizer), alpha=0.1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv1', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv0'), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv1'), alpha=0.1)
     n = tf.nn.max_pool(n, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
     skips.append(n)
 
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv2', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv2'), alpha=0.1)
     n = tf.nn.max_pool(n, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     skips.append(n)
 
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv3', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv3'), alpha=0.1)
     n = tf.nn.max_pool(n, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     skips.append(n)
 
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv4', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv4'), alpha=0.1)
     n = tf.nn.max_pool(n, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     skips.append(n)
 
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv5', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv5'), alpha=0.1)
     n = tf.nn.max_pool(n, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv6', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 48, 3, padding='same', name='enc_conv6'), alpha=0.1)
 
 
     # -----------------------------------------------
     n = upscale2d(n)
     n = tf.concat([n, skips.pop()], axis=-1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv5', kernel_regularizer=regularizer), alpha=0.1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv5b', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv5'), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv5b'), alpha=0.1)
 
     n = upscale2d(n)
     n = tf.concat([n, skips.pop()], axis=-1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv4', kernel_regularizer=regularizer), alpha=0.1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv4b', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv4'), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv4b'), alpha=0.1)
 
     n = upscale2d(n)
     n = tf.concat([n, skips.pop()], axis=-1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv3', kernel_regularizer=regularizer), alpha=0.1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv3b', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv3'), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv3b'), alpha=0.1)
 
     n = upscale2d(n)
     n = tf.concat([n, skips.pop()], axis=-1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv2', kernel_regularizer=regularizer), alpha=0.1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv2b', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv2'), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 96, 3, padding='same', name='dec_conv2b'), alpha=0.1)
 
     n = upscale2d(n)
     n = tf.concat([n, skips.pop()], axis=-1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 64, 3, padding='same', name='dec_conv1a', kernel_regularizer=regularizer), alpha=0.1)
-    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 32, 3, padding='same', name='dec_conv1b', kernel_regularizer=regularizer), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 64, 3, padding='same', name='dec_conv1a'), alpha=0.1)
+    n = tf.nn.leaky_relu(tf.layers.conv2d(n, 32, 3, padding='same', name='dec_conv1b'), alpha=0.1)
 
-    n = tf.layers.conv2d(n, 1, 3, padding='same', name='dec_conv1', kernel_regularizer=regularizer)
+    n = tf.layers.conv2d(n, 1, 3, padding='same', name='dec_conv1')
 
     return x - n
